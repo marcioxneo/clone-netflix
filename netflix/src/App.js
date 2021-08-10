89c8ad53b3494ec401c4b8b36c9360dc9ffbd92b
Marcio Andrade <marcioandr@live.com>
2021-08-10 04:28:09 -0300

    Header

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow'
 import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([])
   const [featuredData, setFeaturedData] =  useState(null) 
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
     const loadAll = async () => {
@@ -29,12 +31,28 @@ export default () => {
     loadAll()
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
   return (
     <div className="page">
      {/* Header 
      Destaque
      Lista 
      Rodapé básico */}
+     <Header black={blackHeader} />
      {featuredData &&
       <FeaturedMovie item={featuredData} />
      }
